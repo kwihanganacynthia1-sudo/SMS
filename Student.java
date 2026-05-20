f80a39da83838a63e3aafced9e50ee54af90ee31
Cynthia1 <kwihanganacynthia1@gmail.com>
2026-05-20 19:56:26 +0200

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,63 @@
+public class Student {
+    String Name;
+    int age;
+    int marks;
+
+    // create constructor
+    public Student(String name, int age, int mark) {
+
+        this.Name;
+        this.age
+        this.marks
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+    public void displayStudentinfo(){
+        System.out.println("StudentName:" + StudentName );
+        System.out.println("age: " +  age);
+        System.out.println("marks:" + marks);
+
+
+    }
+    switch (choise){
+        case1: {
+        if (marks <=99 && marks >=90){
+            System.out.println("A");
+            }
+         else if (marks <=89 && marks >=70){
+                System.out.println("B");
+
+            }
+            else if (marks <=69 && marks >=50){
+
+                System.out.println("C");
+
+        }
+            else {
+            System.out.println("F");
+        }
+    }
+}
+
